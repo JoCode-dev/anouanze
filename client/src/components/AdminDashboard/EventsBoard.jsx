9273036ec3155b70cb7159891e9b6d4b0b822514
JoCode <46027270+JoCode-dev@users.noreply.github.com>
2022-09-16 21:17:48 +0000

    Update - 16-09-2022

--- client/src/components/AdminDashboard/EventsBoard.jsx
+++ client/src/components/AdminDashboard/EventsBoard.jsx
@@ -7,11 +7,95 @@ import { useState } from "react";
 
 const EventsBoard = () => {
   // Afficher / Cacher le formulaire
-  const [toggleForm, setToggleForm] = useState(false);
+  const [toggleForm, setToggleForm] = useState(true);
+
+  const [files, setFiles] = useState([]);
+
+  const [isOk, setIsOk] = useState(false);
+
   const dispatch = useDispatch();
   const events = useSelector((state) => state.events);
+
+  const renderFiles = () => {};
+
+  const submitForm = (e) => {
+    e.preventDefault();
+  };
+
+  const cancelForm = () => {};
+
   return (
     <>
+      {toggleForm && (
+        // Form elements
+        <div className="form-container">
+          <div className="form-group-container">
+            <div className="container-header">
+              <h1>Fiche Évènement</h1>
+
+              <div
+                className="form-closer"
+                onClick={() => setToggleForm(!toggleForm)}
+              >
+                Fermer
+              </div>
+            </div>
+
+            <div className="form-elements-container">
+              <form onSubmit={(e) => submitForm(e)}>
+                <div className="form-group">
+                  <label htmlFor="title">Titre</label>
+                  <input htmlFor="title" placeholder="Titre" />
+                </div>
+                <div className="form-group-group">
+                  <div className="form-group">
+                    <label htmlFor="address">Lieu</label>
+                    <input htmlFor="address" placeholder="Lieu" />
+                  </div>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="description">Description</label>
+                  <textarea htmlFor="description" placeholder="Description" />
+                </div>
+
+                {/** Picture */}
+                <div className="pictures">
+                  <div className="pictures-title">
+                    <h3>Image</h3>
+                  </div>
+                  <div className="pictures-content">
+                    <div className="pictures-blocks-container">
+                      {!isEmpty(files) && <>{renderFiles()}</>}
+
+                      <div className="file-upload">
+                        <input
+                          type="file"
+                          name="file"
+                          //onChange={(e) => fileSelectedHandler(e)}
+                          accept=".jpg, .jpeg, .png"
+                        />
+                        <i className="fa fa-arrow-up"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/** Clergy */}
+
+                <div className="btn-group">
+                  <input type="submit" value="Save" />
+                  {isOk === true && <i className="fa fa-spinner fa-spin"></i>}
+
+                  <input
+                    type="reset"
+                    value="Annuler"
+                    onClick={() => cancelForm()}
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="events-board-container">
         <div className="header">
           <h1>Évènements</h1>
