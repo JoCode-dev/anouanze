2056448be22fe2d896c208cfaf70e401da3f0ee1
JoCode <46027270+JoCode-dev@users.noreply.github.com>
2022-10-04 17:43:10 +0000

    Update 04-10-22

--- client/src/components/Home/Bloc Events/EventsBloc.jsx
+++ client/src/components/Home/Bloc Events/EventsBloc.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const EventsBloc = () => {
-  return <div>EventsBloc</div>;
+  return (
+    <div className="bloc-events-container">
+      <div>
+        <h1>Evènements</h1>
+      </div>
+
+      <div>
+        <h2>C’est pourquoi je vous le dit :</h2>
+        <h2>Tout ce que vous demanderez en priant,</h2>
+        <h2>croyez que vous l’avez réçu et cela vous sera accordé.</h2>
+      </div>
+
+      <div className="ref">
+        <h3>Marc 11:24</h3>
+      </div>
+
+      <NavLink to="/events" className="btn-visit">
+        Voir les annonces
+      </NavLink>
+    </div>
+  );
 };
 
 export default EventsBloc;
